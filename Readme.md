f8c4d37b8db5fa09cf8d19f7a080a907480eed89
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# ASPxGridView - How to expand groups  to a top level 
+
+
+<p>This example demonstrates how to expand groups to a top level.  The <a href="http://documentation.devexpress.com/#AspNet/DevExpressWebASPxGridViewASPxGridView_IsGroupRowtopic"><u>ASPxGridView.IsGroupRow</u></a>, <a href="http://documentation.devexpress.com/#AspNet/DevExpressWebASPxGridViewASPxGridView_GetRowLeveltopic"><u>ASPxGridView.GetRowLevel</u></a> and <a href="http://documentation.devexpress.com/#AspNet/DevExpressWebASPxGridViewASPxGridView_ExpandRowtopic"><u>ASPxGridView.ExpandRow</u></a> methods can be used to accomplish this task.</p><br />
+
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,29 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web.ASPxGridView
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
+	End Sub
+
+
+	Protected Sub grid_DataBound(ByVal sender As Object, ByVal e As EventArgs)
+		If (Not IsPostBack) OrElse (Not IsCallback) Then
+			Dim grid As ASPxGridView = TryCast(sender, ASPxGridView)
+			For i As Integer = 0 To grid.VisibleRowCount - 1
+				If grid.IsGroupRow(i) AndAlso (grid.GetRowLevel(i) = 0) Then
+					grid.ExpandRow(i)
+
+				End If
+			Next i
+		End If
+	End Sub
+End Class
+
